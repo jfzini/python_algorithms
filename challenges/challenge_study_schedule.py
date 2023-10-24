ac49ef678a8be1c4fb5fe6dbd92a4eb49dc6f651
João Felipe Zini <joaofelipezini@gmail.com>
2023-10-24 19:44:09 -0300

    req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    isInt = isinstance(target_time, int)
+    isFloat = isinstance(target_time, float)
+    isListOfTuples = isinstance(permanence_period, list) and all(
+        isinstance(item, tuple) for item in permanence_period
+    )
+    active = 0
+    if (isInt or isFloat) and isListOfTuples:
+        for check_in, check_out in permanence_period:
+            if check_in <= target_time <= check_out:
+                active += 1
+        return active
+    return None
+
+
+print(study_schedule([(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)], 5))
