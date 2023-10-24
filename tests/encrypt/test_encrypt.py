e93b2a799b0e92caddfeecf140ec92ecb387819b
João Felipe Zini <joaofelipezini@gmail.com>
2023-10-24 20:21:35 -0300

    feat: req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+from pytest import raises
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("teste", 2) == "ets_et"
+    assert encrypt_message("teste", 3) == "set_et"
+    assert encrypt_message("teste", -10) == "etset"
+    assert encrypt_message("teste", 10) == "etset"
+
+    with raises(TypeError):
+        encrypt_message("teste", "10")
+        encrypt_message(10, 10)
+        encrypt_message(10, "10")
